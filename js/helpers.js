48ff39f027fe50d324293dae1bb96274211dce23
lexmodo <ahmed.karam@lexmodo.com>
2022-02-06 16:25:41 +0200

    Click and Change Event Are Aded

--- js/helpers.js
+++ js/helpers.js
@@ -95,6 +95,7 @@ class VectorElementCollection extends Array {
         return children
     }
 
+    // needs Handling Id Selector as aparam
     siblings (selector) {
         const siblings = new VectorElementCollection(...this[0].parentElement.children)
         if (selector && selector.startsWith(".")) {
@@ -107,21 +108,80 @@ class VectorElementCollection extends Array {
         }
         return siblings
     }
+
+    /* Start Events */
+    click(callBack) {
+        this.forEach(el => el.addEventListener("click", (e) => callBack(e)))
+    }
+
+    /* Start Events */
+    change(callBack) {
+        this.forEach(el => el.addEventListener("change", (e) => callBack(e)))
+    }
+
+    remove() {
+        this.forEach(el => el.remove())
+    }
+
+    empty () {
+        const all = new VectorElementCollection()
+        this.forEach(el => {
+            all.push(...el.children)
+        })
+        all.remove()
+    }
+
+    replaceAll (selectors) {
+
+    }
 }
 
 
-$(".accordion_toggle_title").on("click", function () {
-    $(".accordion_toggle_title").css({
-        "background-color": "#000",
-        "color": "#fff",
-        "font-size": "20px"
-    })
-    $(this).css("font-size: 80px").css("background-color: aqua").toggleClass("tototototot")
-})
 
-$(".move-kok").append("<p>This Pragraph From append Function Prepend</p>", "prepend")
-$(".move-kok").append("<p>This Pragraph From append Function Append</p>", "append")
-console.log($(".kok").attr("data-add", "global"))
+// $(".accordion_toggle_title").on("click", function () {
+//     $(".accordion_toggle_title").css({
+//         "background-color": "#000",
+//         "color": "#fff",
+//         "font-size": "20px"
+//     })
+//     $(this).css("font-size: 80px").css("background-color: aqua").toggleClass("tototototot")
+// })
 
-console.log($(".accordion_toggle_body").children())
-console.log($(".move-kok").siblings("div"))
+// $(".move-kok").append("<p>This Pragraph From append Function Prepend</p>", "prepend")
+// $(".move-kok").append("<p>This Pragraph From append Function Append</p>", "append")
+// console.log($(".kok").attr("data-add", "global"))
+
+// console.log($(".accordion_toggle_body").children())
+// console.log($(".move-kok").siblings("div"))
+
+// $(".move-kok").click((e) => {
+//     console.log("ahmed is clicked the button")
+//     console.log(e)
+// })
+
+// $(".move-kok").remove()
+
+// $(".move-kok").empty()
+
+
+
+// const countriesSelect = $("#countries")
+// const regionsSelect = $("#regions")
+
+// function countriesData (countryName = "Egypt") {
+//     fetch("https://cdn.lexmodo.com/1/b7a39ac257a2a4ea1ef4f07bb50937801/json/countries_16hxYjuL12fZ.json").then(result => result.json())
+//     .then(countries => {
+//         countries.forEach(country => {
+//             if (country.countryName == countryName) {
+//                 countriesSelect.append(`<option selected>${country.countryName}</option>`)
+//                 regionsSelect.empty()
+//                 country.regions.forEach(region => {
+//                     regionsSelect.append(`<option value="${region.name}">${region.name}</option>`)
+//                 })
+//             } else countriesSelect.append(`<option>${country.countryName}</option>`)
+            
+//         })
+//     })
+// }
+// countriesData()
+// countriesSelect.change((e) => countriesData(e.target.value))
